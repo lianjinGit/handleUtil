dda79a40633990ae84d26ed6a5bec3259c2e0b2f
281475794@qq.com <281475794@qq.com>
2015-06-03 16:51:46 +0800

    注释

--- src/com/handle/util/ErrorCodeMapping.java
+++ src/com/handle/util/ErrorCodeMapping.java
@@ -2,17 +2,17 @@ package com.handle.util;
 
 public class ErrorCodeMapping {
 
-    public static int NO_ERROR = 100000;
+    public static int NO_ERROR = 100000;//没有错误
 
-    public static int ERROR_CODE_USERNAMEPASSWORD_ERROR = 100001;
+    public static int ERROR_CODE_USERNAMEPASSWORD_ERROR = 100001;//用户名密码错误
 
-    public static int ERROR_USERID_REQUIRED = 100002;
-    public static int ERROR_USERNAME_REQUIRED = 100003;
-    public static int ERROR_SEXID_REQUIRED = 100004;
-    public static int ERROR_USERTYPEID_REQUIRED = 100005;
-    public static int ERROR_PASSWORD_REQUIRED = 100006;
+    public static int ERROR_USERID_REQUIRED = 100002;//用户名ID是必须的
+    public static int ERROR_USERNAME_REQUIRED = 100003;//用户名必须的
+    public static int ERROR_SEXID_REQUIRED = 100004;//用户类型是必须的
+    public static int ERROR_USERTYPEID_REQUIRED = 100005;//性别是必须的
+    public static int ERROR_PASSWORD_REQUIRED = 100006;//密码是必须的
 
-    public static int ERROR_REGISTER = 100007;
-    public static int ERROR_DUPLICATED = 100008;
+    public static int ERROR_REGISTER = 100007;//注册错误
+    public static int ERROR_DUPLICATED = 100008;//用户名重复
 
 }
--- src/com/handle/util/RequestResponseCodeMapping.java
+++ src/com/handle/util/RequestResponseCodeMapping.java
@@ -2,10 +2,10 @@ package com.handle.util;
 
 public class RequestResponseCodeMapping {
 
-    public static int LOGIN_RESPONSECODE = 100001;
-    public static int LOGINREQUESTCODE = 100002;
+    public static int LOGIN_RESPONSECODE = 100001;//标示 登录请求
+    public static int LOGINREQUESTCODE = 100002;//标示登录响应
 
-    public static int REGISTER_RESPONSECODE = 100003;
-    public static int REGISTER_REQUESTCODE = 100004;
+    public static int REGISTER_RESPONSECODE = 100003;//标示注册请求
+    public static int REGISTER_REQUESTCODE = 100004;//标示注册响应
 
 }
